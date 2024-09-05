b7e41ad5d16d8c14be2d5ebf6624f48f2a7bc072
sheiyatu <aninwikeziva@gmail.com>
2024-09-05 14:24:11 +0100

    seventh

---
+++ donate.js
@@ -0,0 +1,20 @@
+const recipient = [
+  {
+    id: 1,
+    name: "flood in lambo",
+    desc: "Lamboa community needs your help for crisis management from 3 days of non-stop flooding.",
+    donation: 230,
+  },
+  {
+    id: 2,
+    name:"Tsunami in Malika",
+    desc:"Emergency! A tsunami has just hit Malika, Tarasudi District. Help our affected brothers and sisters.",
+    donation:1090,
+  },
+  {
+    id: 3,
+    name:"Help African Children",
+    desc:"African children need your help to get proper food and water. Prolonged crisis is a real urgency.",
+    donation:"748",
+  }
+];
